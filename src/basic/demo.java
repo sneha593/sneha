e485eb4ce519d0a927d36f05c06eb792e41e466c
sneha593 <114008263+sneha593@users.noreply.github.com>
2022-09-20 21:11:38 +0530

    My First Repository

---
+++ src/basic/demo.java
@@ -0,0 +1,14 @@
+package basic;
+
+public class demo {
+	public static void main(String[] args) {
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]); 
+		System.out.println( args[0] +  "   " +args[1]);
+		System.out.println(a+b);
+
+		System.out.println( "thanku ..");
+	}
+     
+     
+}
